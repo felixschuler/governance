f3e2585607bb1a47cdae0dd371baef97f6c37565
Felix Schuler <felix.slr@icloud.com>
2023-11-25 15:08:14 +0100

    Added OpenZeppelin

---
+++ scripts/audits.py
@@ -0,0 +1,18 @@
+import json
+
+# File path to the audits.json
+file_path = '../src/audits.json'
+
+# Load the JSON data from the file
+with open(file_path, 'r') as file:
+    audits = json.load(file)
+
+# Update the ID for each audit
+for i, audit in enumerate(audits, start=1):
+    audit['id'] = i
+
+# Write the updated data back to the file
+with open(file_path, 'w') as file:
+    json.dump(audits, file, indent=4)
+
+print("Audit IDs have been updated.")
