1142428e2b13265087093987377dd081adefed25
Felix Schuler <felix.slr@icloud.com>
2023-12-27 09:37:33 +0100

    Changes

--- scripts/website/code4rena/code4rena.py
+++ scripts/website/code4rena/code4rena.py
@@ -1,21 +1,59 @@
+# import json
+
+
+# def capitalize_auditee(data):
+#     for item in data:
+#         item["auditee"] = item["auditee"].capitalize()
+#     return data
+
+
+# # Read the existing data
+# with open("code4rena.json", "r") as file:
+#     data = json.load(file)
+
+# # Capitalize the first letter of each auditee
+# updated_data = capitalize_auditee(data)
+
+# # Write the updated data back to the file
+# with open("code4rena.json", "w") as file:
+#     json.dump(updated_data, file, indent=4)
+
+# print("Updated the auditee names in code4rena.json")
+
 import json
+import os
 
+base_dir = "../../../public/reports/code4rena"  # Replace with your base directory path
+output_json = "example.json"
+reports = []
+id_counter = 1
 
-def capitalize_auditee(data):
-    for item in data:
-        item["auditee"] = item["auditee"].capitalize()
-    return data
+# Walking through the directory structure
+for auditee in os.listdir(base_dir):
+    auditee_path = os.path.join(base_dir, auditee)
+    if os.path.isdir(auditee_path):
+        for date in os.listdir(auditee_path):
+            date_path = os.path.join(auditee_path, date)
+            if os.path.isdir(date_path):
+                for file in os.listdir(date_path):
+                    if file.endswith(".pdf"):
+                        report_obj = {
+                            "id": id_counter,
+                            "auditor": "code4rena",
+                            "auditee": auditee,
+                            "date": date,
+                            "report": "https://governance.felixschuler.at/reports/code4rena/"
+                            + auditee
+                            + "/"
+                            + date
+                            + "/"
+                            + file,
+                        }
+                        reports.append(report_obj)
+                        id_counter += 1
 
+# Save the data to a JSON file
+with open(output_json, "w") as json_file:
+    json.dump(reports, json_file, indent=4)
 
-# Read the existing data
-with open("code4rena.json", "r") as file:
-    data = json.load(file)
-
-# Capitalize the first letter of each auditee
-updated_data = capitalize_auditee(data)
-
-# Write the updated data back to the file
-with open("code4rena.json", "w") as file:
-    json.dump(updated_data, file, indent=4)
-
-print("Updated the auditee names in code4rena.json")
+print(f"Data saved to {output_json}")
--- scripts/website/openzeppelin/openzeppelin.py
+++ scripts/website/openzeppelin/openzeppelin.py
@@ -1,62 +1,102 @@
+# import json
+# import os
+# import re
+# import time
+
+# import requests
+# from bs4 import BeautifulSoup
+
+
+# def month_to_number(month_name):
+#     month_dict = {
+#         "January": "01", "February": "02", "March": "03",
+#         "April": "04", "May": "05", "June": "06",
+#         "July": "07", "August": "08", "September": "09",
+#         "October": "10", "November": "11", "December": "12"
+#     }
+#     return month_dict.get(month_name, "01")  # Default to January if not found
+
+# def extract_date_and_auditee(url):
+#     time.sleep(1)
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.text, 'html.parser')
+
+#     auditee = soup.find(id='hs_cos_wrapper_name').text.strip()
+#     span_tags = soup.find_all('span')
+#         # Filter out spans that have a class or an ID
+#     for span in span_tags:
+#         if not span.get('class') and not span.get('id'):
+#             date_text = span.text.strip()
+#             print(date_text)
+#             date_match = re.search(r'(\w+)\s(\d{1,2}),\s(\d{4})', date_text)
+#             if date_match:
+#                 month, _, year = date_match.groups()
+#                 formatted_date = f"{year}-{month_to_number(month)}"
+#                 return auditee, formatted_date
+
+# base_url = 'https://blog.openzeppelin.com/tag/security-audits/page/'
+# auditor = 'OpenZeppelin'
+# audit_reports = []
+
+# for page in range(1, 18):  # 17 pages in total
+#     url = f"{base_url}{page}"
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.text, 'html.parser')
+
+#     for link in soup.find_all("a", class_="blog-listing__post-title-link"):
+#         href = link.get('href')
+#         if href:
+#             auditee, date = extract_date_and_auditee(href)
+#             audit_reports.append({
+#                 "id": len(audit_reports) + 1,
+#                 "auditor": auditor,
+#                 "auditee": auditee,
+#                 "date": date,
+#                 "report": href
+#             })
+
+# # Save the data to oz.json in the root directory
+# with open('oz.json', 'w') as file:
+#     json.dump(audit_reports, file, indent=4)
+
+# print("Data saved to oz.json")
+
 import json
 import os
-import re
-import time
 
-import requests
-from bs4 import BeautifulSoup
+base_dir = (
+    "../../../public/reports/OpenZeppelin"  # Replace with your base directory path
+)
+output_json = "example.json"
+reports = []
+id_counter = 1
 
+# Walking through the directory structure
+for auditee in os.listdir(base_dir):
+    auditee_path = os.path.join(base_dir, auditee)
+    if os.path.isdir(auditee_path):
+        for date in os.listdir(auditee_path):
+            date_path = os.path.join(auditee_path, date)
+            if os.path.isdir(date_path):
+                for file in os.listdir(date_path):
+                    if file.endswith(".pdf"):
+                        report_obj = {
+                            "id": id_counter,
+                            "auditor": "OpenZeppelin",
+                            "auditee": auditee,
+                            "date": date,
+                            "report": "https://governance.felixschuler.at/reports/OpenZeppelin/"
+                            + auditee
+                            + "/"
+                            + date
+                            + "/"
+                            + file,
+                        }
+                        reports.append(report_obj)
+                        id_counter += 1
 
-def month_to_number(month_name):
-    month_dict = {
-        "January": "01", "February": "02", "March": "03",
-        "April": "04", "May": "05", "June": "06",
-        "July": "07", "August": "08", "September": "09",
-        "October": "10", "November": "11", "December": "12"
-    }
-    return month_dict.get(month_name, "01")  # Default to January if not found
+# Save the data to a JSON file
+with open(output_json, "w") as json_file:
+    json.dump(reports, json_file, indent=4)
 
-def extract_date_and_auditee(url):
-    time.sleep(1)
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    auditee = soup.find(id='hs_cos_wrapper_name').text.strip()
-    span_tags = soup.find_all('span')
-        # Filter out spans that have a class or an ID
-    for span in span_tags:
-        if not span.get('class') and not span.get('id'):
-            date_text = span.text.strip()
-            print(date_text)
-            date_match = re.search(r'(\w+)\s(\d{1,2}),\s(\d{4})', date_text)
-            if date_match:
-                month, _, year = date_match.groups()
-                formatted_date = f"{year}-{month_to_number(month)}"
-                return auditee, formatted_date
-
-base_url = 'https://blog.openzeppelin.com/tag/security-audits/page/'
-auditor = 'OpenZeppelin'
-audit_reports = []
-
-for page in range(1, 18):  # 17 pages in total
-    url = f"{base_url}{page}"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    for link in soup.find_all("a", class_="blog-listing__post-title-link"):
-        href = link.get('href')
-        if href:
-            auditee, date = extract_date_and_auditee(href)
-            audit_reports.append({
-                "id": len(audit_reports) + 1,
-                "auditor": auditor,
-                "auditee": auditee,
-                "date": date,
-                "report": href
-            })
-
-# Save the data to oz.json in the root directory
-with open('oz.json', 'w') as file:
-    json.dump(audit_reports, file, indent=4)
-
-print("Data saved to oz.json")
+print(f"Data saved to {output_json}")
